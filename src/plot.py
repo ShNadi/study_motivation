9dd30fbf99d588131e1f14e29bd2a5cbba9a1218
Shiva Nadi <s.nadi@uu.nl>
2021-03-17 13:00:23 +0100

    correlation_matrix added

--- src/plot.py
+++ src/plot.py
@@ -20,8 +20,22 @@ def plot_var(df, type='boxplot', Y='age', X='bsa_dummy', HUE= None):
         bplot.tick_params(labelsize=8)
         plt.show()
 
+    if type == 'correlation_matrix':
+        f = plt.figure(figsize=(8, 10))
+        plt.matshow(df.corr(), fignum=f.number)
+        plt.xticks(range(df.select_dtypes(['number']).shape[1]), df.select_dtypes(['number']).columns, fontsize=10,
+                   rotation=90)
+        plt.yticks(range(df.select_dtypes(['number']).shape[1]), df.select_dtypes(['number']).columns, fontsize=10)
+        cb = plt.colorbar()
+        cb.ax.tick_params(labelsize=14)
+        plt.title('Correlation Matrix', fontsize=16,  y=-0.01);
+        plt.show()
+
 
 
 if __name__ == "__main__":
     df = load_dataset.read_df()
-    plot_var(df, type='boxplot', Y='age', X='bsa_dummy', HUE='year')
+    # plot_var(df, type='boxplot', Y='age', X='bsa_dummy', HUE='year')
+    plot_var(df, type='correlation_matrix')
+
+
