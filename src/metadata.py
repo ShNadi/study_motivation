b8faa2d4bd775d41686ae709e057f3ef0a3bd645
Shiva Nadi <s.nadi@uu.nl>
2021-03-18 15:17:10 +0100

    code style checked with flake 8

--- src/metadata.py
+++ src/metadata.py
@@ -2,16 +2,14 @@ import pandas as pd
 import string
 
 
-def count_punctuation (df):
-    count = lambda l1,l2: sum([1 for x in l1 if x in l2])
+def count_punctuation(df):
+	# Counts the number of punctuations in the text
+	count = lambda l1, l2: sum([1 for x in l1 if x in l2])
     df['count_punct'] = df.motivation.apply(lambda s: count(s, string.punctuation))
     df.to_csv(r'..\data\processed\motivation_liwc_meta.csv', index=False)
 
 
-
-
+# Add extra columns to the dataset
 if __name__ == "__main__":
     df = pd.read_csv(r'..\data\processed\liwc_results.csv')
-    print(count_punctuation (df))
-
-
+    print(count_punctuation(df))
