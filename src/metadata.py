9a1a3278d6da2bfc1365150b6f144d635768ab5b
Shiva Nadi <s.nadi@uu.nl>
2021-03-22 15:19:45 +0100

    count_stopwords function is added to the metadata.py

--- src/metadata.py
+++ src/metadata.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import string
+from nltk.corpus import stopwords
 
 
 def count_punctuation(df):
@@ -9,7 +10,25 @@ def count_punctuation(df):
     df.to_csv(r'..\data\processed\motivation_liwc_meta.csv', index=False)
 
 
+def count_stopwords(df):
+    # Counts the number of stopwords in the text
+    stopword_list = set(stopwords.words('Dutch'))
+    df['count_stopwords'] = df['motivation'].str.split().apply(lambda x: len(set(x) & stopword_list))
+    df.to_csv(r'..\data\processed\motivation_liwc_meta.csv', index=False)
+
+
+
+
 # Add extra columns to the dataset
 if __name__ == "__main__":
-    df = pd.read_csv(r'..\data\processed\liwc_results.csv')
-    print(count_punctuation(df))
+    # df = pd.read_csv(r'..\data\processed\liwc_results.csv')
+    # print(count_punctuation(df))
+
+    df = pd.read_csv(r'..\data\processed\motivation_liwc_meta.csv')
+    count_stopwords(df)
+
+
+
+
+
+
