c091f7e7e0cc56d84deb1d00a34474c623e60c4d
Shiva Nadi <s.nadi@uu.nl>
2021-03-31 17:51:18 +0200

    comments are added and lemmatization is commented

--- src/lda_document_topic.py
+++ src/lda_document_topic.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from sklearn.utils import shuffle
 import nltk
 nltk.download('stopwords')
@@ -6,33 +5,33 @@ from nltk.corpus import stopwords
 import re
 import numpy as np
 import pandas as pd
-from pprint import pprint
-
 # Gensim
 import gensim
 import gensim.corpora as corpora
 from gensim.utils import simple_preprocess
-from gensim.models import CoherenceModel
 # spacy for lemmatization
 import spacy
-
-# Plotting tools
-import pyLDAvis
-import pyLDAvis.gensim_models as gensimvis
-import matplotlib.pyplot as plt
-
 # Enable logging for gensim - optional
 import logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.ERROR)
 
+# Filter warning messages
 import warnings
 warnings.filterwarnings("ignore",category=DeprecationWarning)
 
+# Download list of Dutch stopwords
 stop_words = stopwords.words('Dutch')
 
 
-
 def lda_document_topic_distribution():
+    """ Document Topic Distribuation
+
+     Using LDA, this function distinguish 10 topic category from the text
+     and calculates document-topic matrix.
+
+    """
+
+    # Load and shuffle the dataset
     df = pd.read_csv(r'..\data\processed\motivation_liwc_meta_pos.csv', usecols=['motivation', 'bsa_dummy'])
     df = shuffle(df, random_state=100)
 
@@ -45,17 +44,16 @@ def lda_document_topic_distribution():
     # Remove distracting single quotes
     data = [re.sub(r"\'", "", sent) for sent in data]
 
-
-
+    # Tokenize words and Clean-up text
     data_words = list(sent_to_words(data))
 
     # Build the bigram and trigram models
     bigram = gensim.models.Phrases(data_words, min_count=5, threshold=100)  # higher threshold fewer phrases.
-    trigram = gensim.models.Phrases(bigram[data_words], threshold=100)
+    # trigram = gensim.models.Phrases(bigram[data_words], threshold=100)
 
     # Faster way to get a sentence clubbed as a trigram/bigram
     bigram_mod = gensim.models.phrases.Phraser(bigram)
-    trigram_mod = gensim.models.phrases.Phraser(trigram)
+    # trigram_mod = gensim.models.phrases.Phraser(trigram)
 
     # Remove Stop Words
     data_words_nostops = remove_stopwords(data_words)
@@ -79,7 +77,7 @@ def lda_document_topic_distribution():
     # Term Document Frequency
     corpus = [id2word.doc2bow(text) for text in texts]
 
-    # Build LDA model
+    # Build LDA model- number of topics=10
     lda_model2 = gensim.models.ldamodel.LdaModel(corpus=corpus,
                                                  id2word=id2word,
                                                  num_topics=10,
@@ -102,16 +100,39 @@ def lda_document_topic_distribution():
 
 
 def sent_to_words(sentences):
+    """ Tokenize words and Clean-up text
+
+This function tokenize each sentence into a list of words,
+removing punctuations and unnecessary characters altogether.
+
+    :param sentences: list
+    :type sentences: list
+    """
     for sentence in sentences:
         yield(gensim.utils.simple_preprocess(str(sentence), deacc=True))  # deacc=True removes punctuations
 
 
-# Define functions for stopwords, bigrams, trigrams and lemmatization
 
 def remove_stopwords(texts):
+    """ Remove Stopwords
+
+    :param texts: input text
+    :type texts: list
+    :return: the text without stopwords
+    :rtype: list
+    """
     return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
 
 def make_bigrams(texts, bigram_mod):
+    """
+This function makes bigrams form the text data.
+    :param texts:
+    :type texts: list
+    :param bigram_mod:
+    :type bigram_mod: list
+    :return:
+    :rtype: data_words_bigrams
+    """
     return [bigram_mod[doc] for doc in texts]
 
 # def make_trigrams(texts, bigram_mod, trigram_mod):
@@ -124,6 +145,7 @@ def lemmatization(texts, nlp, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
         doc = nlp(" ".join(sent))
         texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
     return texts_out
+
 
 if __name__ == "__main__":
     lda_document_topic_distribution()
