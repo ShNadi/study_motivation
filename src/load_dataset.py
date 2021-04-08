27a7ab7571aa9f00f9f03d8cfa38c0bd902a0206
Shiva Nadi <s.nadi@uu.nl>
2021-04-08 10:34:02 +0200

    comment added

--- src/load_dataset.py
+++ src/load_dataset.py
@@ -1,15 +1,27 @@
 import pandas as pd
 
 def read_df( status = 'read'):
+    """
+This function reads datasets(2014, 2015), matches the name of columns, concatenate, and make dataset ready for further
+analysis
+    :param status: takes two variable, 'prepare': to match the name of columns and concatenate the datasets and write it
+    in '..\data\processed\motivation.csv'. 'read': to read the dataset.
+    :type status: string
+    :return: concatenated dataset
+    :rtype: DataFrame
+    """
     if status == 'prepare':
-        # Read df_2014 and df_2015
+        # Specify column names
         columns = ['studentnr_crypt', 'cohort', 'program', 'field', 'motivation',
                         'prior_educ', 'previously_enrolled', 'multiple_requests', 'age',
                         'gender', 'HSGPA', 'interest', 'ase', 'reenrolled', 'bsa_dummy']
-
+        # Read dataset_2014
         df_2014 = pd.read_csv(r'..\data\raw\motivatoin_2014.csv')
+        # Add year as a column to the dataset
         df_2014 ['year'] = '2014'
+        # Read dataset_2015
         df_2015 = pd.read_csv(r'..\data\raw\motivation_2015.csv', usecols = columns)
+        # Add year as a column to the dataset
         df_2015['year'] = '2015'
 
         df_2014.rename(columns={"COHORT": "cohort"}, inplace=True)
