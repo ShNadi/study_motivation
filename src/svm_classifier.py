803220938babb694f74125927b92c312f0cab022
Shiva Nadi <s.nadi@uu.nl>
2021-04-13 11:55:37 +0200

    check for separate datasets

--- src/svm_classifier.py
+++ src/svm_classifier.py
@@ -15,7 +15,7 @@ from contextlib import redirect_stdout
 import pickle
 
 
-def svm_func(df):
+def svm_func(df, filename):
 
     # keep test_set apart
     df_train, df_test = train_test_split(df, test_size=0.25, stratify=df['bsa_dummy'], shuffle=True,
@@ -25,8 +25,8 @@ def svm_func(df):
     X_test = df_test['motivation']
     y_test = df_test['bsa_dummy']
 
-    df_train.to_csv(r'..\data\processed\train_set.csv', index=False)
-    df_test.to_csv(r'..\data\processed\test_set.csv', index=False)
+    # df_train.to_csv(r'..\data\processed\train_set.csv', index=False)
+    # df_test.to_csv(r'..\data\processed\test_set.csv', index=False)
     print("test_set and train_set are stored in 'data\preprocessed' folder.")
 
     # # separate train/evaluation set
@@ -38,7 +38,7 @@ def svm_func(df):
 
     pipe = make_pipeline(TfidfVectorizer(lowercase=True, stop_words=stopword_list), SVC(class_weight='balanced'))
     scores = cross_val_score(pipe, X_train, y_train, cv=5)
-    pickle.dump(pipe, open("../data/model/svm_crossval_model.sav", 'wb'))
+    pickle.dump(pipe, open("../data/model/"+ filename+ "_model.sav", 'wb'))
 
     print('5-fold cross validation scores:', scores)
     print('average of 5-fold cross validation scores:', scores.mean())
@@ -49,7 +49,7 @@ def svm_func(df):
     cm = confusion_matrix(y_test, predictions)
     print(classification_report(y_test, predictions))
 
-    with open(r'../results/output/classification_reports/svm_cross_val.txt', 'w') as f:
+    with open(r'../results/output/classification_reports/'+ filename+'.txt', 'w') as f:
         with redirect_stdout(f):
             print('5-fold cross validation scores:', scores)
             # print('/n')
@@ -64,5 +64,22 @@ def svm_func(df):
 
 
 if __name__=='__main__':
-    df = pd.read_csv('../data/processed/motivation.csv')
-    svm_func(df)
+    # df = pd.read_csv('../data/processed/motivation.csv')
+    # svm_func(df, 'svm_cross_val')
+
+
+    # df_2014 = pd.read_csv('../data/raw/motivatoin_2014.csv')
+    # df_2014['bsa_dummy'] = df_2014['bsa_dummy'].replace({0: 1, 1: 0})
+    # df_2014['reenrolled'] = df_2014['reenrolled'].replace({0: 1, 1: 0})
+    # svm_func(df_2014, 'svm_df_2014')
+
+    # df_2015 = pd.read_csv('../data/raw/motivation_2015.csv')
+    # df_2015.dropna(subset=['reenrolled', 'bsa_dummy', 'motivation'], inplace=True)
+    # df_2015['bsa_dummy'] = df_2015['bsa_dummy'].replace({0: 1, 1: 0})
+    # df_2015['reenrolled'] = df_2015['reenrolled'].replace({0: 1, 1: 0})
+    # svm_func(df_2015, 'svm_df_2015')
+
+    df_2016 = pd.read_csv('../data/raw/motivation_2016.csv')
+    df_2016.dropna(subset=['bsa_dummy', 'motivation'], inplace=True)
+    df_2016['bsa_dummy'] = df_2016['bsa_dummy'].replace({0: 1, 1: 0})
+    svm_func(df_2016, 'svm_df_2016')
